c401a479a73f24f52e2d968019d287849b0b7aea
Noah Isaacson <clewfirst@gmail.com>
2013-05-15 11:51:20 -0400

    fixed send request bug

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 #! /usr/bin/env node
+var inspect = require('eyespect').inspector();
 var argv = require('optimist').demand(['host', 'port']).argv
 var host = argv.host
 var port = argv.port
 var requestUntilDone = require('./lib/requestUntilDone')
-requestUntilDone(host, port)
+requestUntilDone(host, port, function (err, reply) {
+  if (err) {
+    inspect(err, 'request spawn error')
+    return
+  }
+  inspect(reply,'spawn completed')
+})
