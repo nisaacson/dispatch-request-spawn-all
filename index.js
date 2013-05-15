cb78ca2fb43754398fbac419794d2f0f312c5adf
Noah Isaacson <clewfirst@gmail.com>
2013-05-15 11:41:21 -0400

    fix require path

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 var argv = require('optimist').demand(['host', 'port']).argv
 var host = argv.host
 var port = argv.port
-var requestUntilDone = require('./requestUntilDone')
+var requestUntilDone = require('./lib/requestUntilDone')
 requestUntilDone(host, port)
